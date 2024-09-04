2776256899fa90b6ced6ff26fe57a1cb9d6b9084
Shans8 <muhammadihsan0328@gmail.com>
2024-09-04 15:35:20 +0700

    feat: dashboard e-commerce

--- HTML + JS/ecommerce/js/jdc.js
+++ HTML + JS/ecommerce/js/jdc.js
@@ -38,6 +38,21 @@ const fetchProducts = async (category) => {
   }
 }
 
+const renderCard = (container, card, products) => {
+  container.innerHTML = ''
+
+  products.slice(0, 3).forEach((clothing) => {
+    const clonedClothingCard = card.cloneNode(true);
+
+    clonedClothingCard.querySelector('h4').innerText = clothing.title;
+    clonedClothingCard.querySelector('p').querySelector('span').innerText = clothing.price;
+    clonedClothingCard.querySelector('img').src = clothing.image;
+    clonedClothingCard.querySelector('img').style.width = '250px';
+
+    container.appendChild(clonedClothingCard);
+  })
+}
+
 const renderDashboard = async () => {
   try {
     const menClothings = await fetchProducts(`men's%20clothing`);
@@ -45,10 +60,20 @@ const renderDashboard = async () => {
     const electronics = await fetchProducts(`electronics`);
     const jewelry = await fetchProducts(`jewelery`);
 
-    console.log(menClothings);
-    console.log(womenClothings);
-    console.log(electronics);
-    console.log(jewelry);
+    const mixedClothings = [...menClothings, ...womenClothings];
+    
+    const clothingContainer = document.querySelector('[fetched-products=wrapper]');
+    const clothingCard = document.querySelector('[fetched-products=item]').cloneNode(true);
+    
+    const electronicsContainer = document.querySelector('#electronic_main_slider').querySelector('.row');
+    const electronicsCard = document.querySelector('.box_main').parentElement.cloneNode(true);
+
+    const jeweleryContainer = document.querySelector('#jewellery_main_slider').querySelector('.row');
+    const jeweleryCard = document.querySelector('.box_main').parentElement.cloneNode(true);
+
+    renderCard(clothingContainer, clothingCard, mixedClothings);
+    renderCard(electronicsContainer, electronicsCard, electronics);
+    renderCard(jeweleryContainer, jeweleryCard, jewelry);
   } catch (error) {
     console.log(error);
   }
